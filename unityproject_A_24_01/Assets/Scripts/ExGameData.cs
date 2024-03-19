3478c691a975ecb1ebc64e9992455e25641ebbf1
vbv963 <vbv963@gmail.com>
2024-03-19 11:15:17 +0900

    [Feat] Class 관련 소스 샘플 테스트 코딩

---
+++ unityproject_A_24_01/Assets/Scripts/ExGameData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ExGameData" , fileName = "New ExGameData" , order = 50)]
+public class ExGameData : ScriptableObject   //스크립터블 오브젝트 상속
+{
+    //게임 관련 변수들 추가
+    public string gameName;
+    public int gameScore;
+    public bool isGameActive;
+}
---
+++ unityproject_A_24_01/Assets/Scripts/ExPlayer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExPlayer : MonoBehaviour
+{
+    private int health = 100;     //플레이어의 체력
+
+    //플레이어가 피해를 받을 때 호출되는 함수
+    public void TakeDamage(int damage)
+    {
+        //플레이어의 체력 감소
+        health -= damage;
+
+        Debug.Log("플레이어 피격. 남은 체력 : " + health);
+
+        //체력이 0이하로 떨어졌을 때 플레이어의 사망 처리
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+    
+    //플레이어가 사망했을 때 호출 되는 함수
+    private void Die()
+    {
+        Debug.Log("플레이어가 사망했습니다.");
+        //사망 처리 로직 추가
+    }
+}
