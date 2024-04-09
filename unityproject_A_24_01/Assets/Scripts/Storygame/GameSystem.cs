d5f2b4687b8e69395f58c3de43d5f81d6369a97c
vbv963 <vbv963@gmail.com>
2024-04-09 09:48:41 +0900

    [Feat] 게임 시스템 업데이트

--- unityproject_A_24_01/Assets/Scripts/Storygame/GameSystem.cs
+++ unityproject_A_24_01/Assets/Scripts/Storygame/GameSystem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;     //에디터 
 using System.Text;     // 텍스트 사용 
+using UnityEngine.UI;  //UI 사용하기 위해
 
 namespace STORYGAME  //이름 충돌 방지
 {
@@ -20,6 +21,22 @@ namespace STORYGAME  //이름 충돌 방지
             {
                 gameSystem.ResetStoryModels();
             }
+
+            if (GUILayout.Button("Assing Text Component by Name"))    //버튼을 생성(UI 컴포넌트를 불러온다)
+            {
+                //오브젝트 이름으로 Text 컴포넌트 찾기
+                GameObject textObject = GameObject.Find("StoryTextUI");
+                if (textObject != null)
+                {
+                    Text textComponent = textObject.GetComponent<Text>();
+                    if (textComponent != null)
+                    {
+                        //Text Component 할당
+                        gameSystem.textComponent = textComponent;
+                        Debug.Log("Text Component assigned Successfully");
+                    }
+                }
+            }
         }
     }
 #endif
@@ -27,13 +44,44 @@ namespace STORYGAME  //이름 충돌 방지
     public class GameSystem : MonoBehaviour
     {
         public static GameSystem instance;    //간단한 싱글톤 화
+        public Text textComponent = null;
+
+        public float delay = 0.1f;            //각 글자가 나타나는 데 걸리는 시간
+        public string fullText;               //전체 표시할 텍스트
+        public string currentText = "";       //현재가지 표시된 텍스트
+
+        public enum GAMESTATE                //상태값 설정 열거형
+        {
+            STORYSHOW,
+            WAITSELECT,
+            STORYEND,
+            ENDMODE
+        }
+
+        public GAMESTATE currentState;
+        public StoryTableObject[] storyModels;    //기존에 있던것 모델들 소스코드 위치 이동
+        public StoryTableObject currentModels;    //현재 스토리 모델 객체
+        public int currentStoryIndex;             //스토리 모델 인덱스
 
         private void Awake()
         {
             instance = this;
         }
 
-        public StoryTableObject[] storyModels;
+        public void Start()                  //게임 시작시
+        {
+            StartCoroutine(ShowText());      //텍스트를 보여준다
+        }
+
+        IEnumerator ShowText()
+        {
+            for (int i = 0; i <= currentModels.storyText.Length; i++)
+            {
+                currentText = currentModels.storyText.Substring(0, i);
+                textComponent.text = currentText;
+                yield return new WaitForSeconds(delay);
+            }
+        }
 
 # if UNITY_EDITOR
         [ContextMenu("Reset Stroy Models")]
